743436875e2ab9c45dcb01e2636ee0a48e97f4e7
Jakh Daven <tuxcanfly@gmail.com>
2010-10-02 23:27:57 +0530

    Added Licence

---
+++ README.md
@@ -0,0 +1,12 @@
+pygtk-comics is a really simple GTK+ app which shows your favorite comics
+on the desktop.
+
+Right now it supports:
+
+* Calvin and Hobbes
+* XKCD
+
+It is really simple to add another comic, you just need to specify the name,
+author and a way to get the latest comic image url. Take a look at the code for
+examples.
+
--- comics.py
+++ comics.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python
 
+# Copyright 2010 Jakh Daven<tuxcanfly@gmail.com>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
 import pygtk
 pygtk.require('2.0')
 import gtk
@@ -78,7 +94,7 @@ class BaseComicsPlugin(object):
 
 class CalvinAndHobbesPlugin(BaseComicsPlugin):
     comic_name = "Calvin and Hobbes"
-    comic_author = "Bill Waterson"
+    comic_author = "Bill Watterson"
 
     def get_comic_url(self, date=datetime.today()):
         return "http://picayune.uclick.com/comics/ch/%s/ch%s.gif" %(
